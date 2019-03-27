f851944379bc1c310b87479a38e65b3424b450d6
ppizarror <pablo.pizarro@ing.uchile.cl>
2019-03-27 07:38:34 +0100

    Elimina archivos sin usar

--- Programa_TEFAME/Modelo_Lib/Elementos_Lib/graficarElemento.m
+++
@@ -1,9 +0,0 @@
-function graficarElemento(coord1, coord2, tipoLinea, grosorLinea)
-%GRAFICARELEMENTO Summary of this function goes here
-%   Detailed explanation goes here
-if length(coord1) == 2
-    plot([coord1(1), coord2(1)], [coord1(2), coord2(2)], tipoLinea, 'LineWidth', grosorLinea);
-else
-    plot3([coord1(1), coord2(1)], [coord1(2), coord2(2)], [coord1(3), coord2(3)], tipoLinea, 'LineWidth', grosorLinea);
-end
-end
