19c53d0a78a0ef5384126c1499ebdfbdec4edb5b
ppizarror <pablo.pizarro@ing.uchile.cl>
2019-06-03 19:50:09 -0400

    Mejora t4

--- test/modal/fourier/DFT.m
+++ test/modal/fourier/DFT.m
@@ -7,19 +7,27 @@ function [f, fftt, ts] = DFT(FS, senal)
 %       fftt = Fast Fourier Transform de la señal
 %       ts = vector de tiempo de la señal
 % -------------------------------------------------------------------
+
 % Intervalo de tiempo
 dt = 1 / FS;
+
 % Tamaño de la señal
 N = length(senal);
-L = dt*N;
+L = dt * N;
+
 % Numero de elementos de la señal
 Nfft = 2^nextpow2(N);
+
 % Vector de frecuencia
-df = FS/Nfft;
-fn = FS/2;                       % Nyquist cut-off frequency.
+df = FS / Nfft;
+fn = FS / 2; % Nyquist cut-off frequency
 f = -fn:df:fn - df;
+
 % Vector de tiempo
-ts = 0:dt:L - dt ; ts = ts';
+ts = 0:dt:L - dt;
+ts = ts';
+
 % FAST FOURIER TRANSFORM
-fftt = fft(senal, Nfft)./Nfft;
+fftt = fft(senal, Nfft) ./ Nfft;
+
 end
--- test/modal/fourier/respfrec.m
+++ test/modal/fourier/respfrec.m
@@ -1,4 +1,4 @@
-function [d, v , a, f, Pw, FRF] = respfrec(m, T, beta, P, FS)
+function [d, v, a, f, Pw, FRF] = respfrec(m, T, beta, P, FS)
 % [d, v , a, P, Pw, FRF] = respfrec(m, T, beta, P, FS)
 % Genera respuesta de oscilador en el espacio de la frecuencia
 %   Parametros de entrada:
@@ -10,55 +10,55 @@ function [d, v , a, f, Pw, FRF] = respfrec(m, T, beta, P, FS)
 %         d: respuesta de desplazamiento
 %         Pw: fft de P
 % ------------------------------------------------------------------
+
 %% Datos de entrada
 % Longitud vector de entrada
 nP = length(P);
 
 % Frecuencia natural y rigidez
-fo = 1/T;
-k = m*(2*pi/T)^2; %REVISAR
+fo = 1 / T;
+k = m * (2 * pi / T)^2; % REVISAR
 
 %% Señal en el espacio de la frecuencia
 Pw = fft(P);
 
 %% Definicion de simetria de FRF
-if ~any(any(imag(P) ~=0))
+if ~any(any(imag(P) ~= 0))
     if rem(nP, 2)
-        select = (1:(nP + 1)/2)';
+        select = (1:(nP + 1) / 2)';
     else
-        select = (1:nP/2 + 1)';
+        select = (1:nP / 2 + 1)';
     end
 else
     select = (1:nP)';
 end
-f = (select - 1)*FS/nP;
+f = (select - 1) * FS / nP;
 
 %% Funcion de respuesta
 FRF = zeros(nP, 1);
-fratio = f/fo;
+fratio = f / fo;
 unos = ones(length(f), 1);
-% Para una señal compleja.
-FRF(select) = (unos/k)./(unos - (fratio).^2 + ...
-    (1i*2*beta).*(fratio));
 
+% Para una señal compleja
+FRF(select) = (unos / k) ./ (unos - (fratio).^2 + ...
+    (1i * 2 * beta) .* (fratio));
 %% Correccion para doble sidedspectra
-
 % Si no es necesario corregir para el otro lado
 if ~any(any(imag(P) ~= 0))
     % correccion de frecuencia
-    f = [f; zeros(nP - length(f), 1)];
+    f = [f; zeros(nP-length(f), 1)];
     if rem(nP, 2)
-        FRF(select(end)+1:end) = conj(FRF(((nP + 1)/2):-1:2));
+        FRF(select(end)+1:end) = conj(FRF(((nP + 1) / 2):-1:2));
         % Simetria completa
-        f(select(end) + 1:end) = -f(((nP + 1)/2):-1:2);
+        f(select(end)+1:end) = -f(((nP + 1) / 2):-1:2);
     else
-        FRF(select(end) + 1:end) = conj(FRF(nP/2:-1:2)); %Par
-        f(select(end) + 1:end) = f((nP/2):-1:2);
+        FRF(select(end)+1:end) = conj(FRF(nP/2:-1:2)); %Par
+        f(select(end)+1:end) = f((nP / 2):-1:2);
     end
 end
 
 d = real(ifft(FRF.*Pw));
-v = real(ifft((1i*f*2*pi).*FRF.*Pw));
-a = real(ifft((1i*f*2*pi).^2.*FRF.*Pw));
-    
+v = real(ifft((1i * f * 2 * pi).*FRF.*Pw));
+a = real(ifft((1i * f * 2 * pi).^2.*FRF.*Pw));
+
 end
